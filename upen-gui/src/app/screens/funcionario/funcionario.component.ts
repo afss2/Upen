be3a7582a7a2546071aa2f00ad3fd64de1ea4e76
afss2 <afss2@cin.ufpe.br>
2020-11-08 16:47:09 -0300

    implementacao de cadastrofuncionario, service e components para preparar cenario de adicao de funcionario

--- upen-gui/src/app/screens/funcionario/funcionario.component.ts
+++ upen-gui/src/app/screens/funcionario/funcionario.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Funcionario } from '../../../../../common/funcionario'
+import { FuncionarioService } from '../../services/FuncionarioService/funcionario.service'
 
 @Component({
   selector: 'app-funcionario',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./funcionario.component.css']
 })
 export class FuncionarioComponent implements OnInit {
+  funcionario: Funcionario = new Funcionario;
+  funcL: Funcionario[] = [];
+  cpfduplicado: boolean = false;
 
-  constructor() { }
+  constructor(private funcService: FuncionarioService) { }
+
+  criarFuncionario(func: Funcionario) {
+      this.funcService.criarFuncionario(func).subscribe (
+        afterCheck => {
+          if (afterCheck) {
+            this.funcL.push(afterCheck);
+            this.funcionario = new Funcionario;
+          } else {
+            this.cpfduplicado = true;
+          }
+        }
+      )
+  }
+
 
   ngOnInit(): void {
+    this.funcService.getFuncionarios().subscribe(
+      funlist => {this.funcL = funlist;}
+    )
   }
+
 
 }
